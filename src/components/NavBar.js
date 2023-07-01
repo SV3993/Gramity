0f2962f02a6f88dd504a05d7ec33123ed375d875
SV3993 <114180037+SV3993@users.noreply.github.com>
2023-07-01 14:20:16 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import propTypes from 'prop-types'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
 export default function NavBar(props) {
   // function switchMode(){
@@ -15,17 +15,17 @@ export default function NavBar(props) {
     <div className="header1">
       <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme={props.mode}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">{props.title}</Link>
+          <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/About">About</Link>
+                <a className="nav-link active" aria-current="page" href="/About">About</a>
               </li>
               {/* <li className="nav-item dropdown">
                 <a className="nav-Link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
